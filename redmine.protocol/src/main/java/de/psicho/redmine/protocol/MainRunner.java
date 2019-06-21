f3ef4274bbc8a5aba97f466d991361aee2998bfc
Markus Meier <markus.d.meier@gmail.com>
2019-06-21 13:34:14 +0200

    refactor

--- redmine.protocol/src/main/java/de/psicho/redmine/protocol/MainRunner.java
+++ redmine.protocol/src/main/java/de/psicho/redmine/protocol/MainRunner.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainRunner {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(MainRunner.class, args);
     }
 }
